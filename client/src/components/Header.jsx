7570aebe9588affceb2053ec9416263fc9219930
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2023-12-09 17:04:49 +0100

    add navbar

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,47 @@
+import './Header.css'
+import WorkIcon from '@mui/icons-material/Work';
+import Paper from '@mui/material/Paper';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import GroupIcon from '@mui/icons-material/Group';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+const Header = () => {
+
+  return (
+    <div className="header">
+      <div className="header__left">
+            <WorkIcon sx={{color: "#0e1344"}} style={{ fontSize: 38 }} />
+            <div className="header__search">
+            <Paper
+                component="form"
+                sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 300 , height: 40 }}
+                >
+                    <InputBase
+                        sx={{ ml: 1, flex: 1 }}
+                        placeholder="Search"
+                        inputProps={{ 'aria-label': 'search' }}
+                    />
+                    <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                        <SearchIcon />
+                    </IconButton>
+
+            </Paper>
+            </div>
+      </div>
+      <div className="header__right">
+            <HeaderOption title="Home" Icon={HomeIcon} />
+            <HeaderOption title="My Network" Icon={GroupIcon} />
+            <HeaderOption title = "Jobs" Icon={ LocalOfferIcon } />
+            <HeaderOption title = "Notifications" Icon={NotificationsIcon} />
+            <HeaderOption title="me" avatar="https://ibb.co/pX1K7WR" />
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,47 +1,22 @@
-import React , { PureComponent } from 'react'
-import {  Main , Container, Cards , Card , Title} from './Dashboard.styles'
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import Navbar from '../components/Navbar';
-import MUINavbar from '../components/MUINavbar';
+import Header from "../components/Header"
 
 
 const Dashboard = () => {
 
-    const createData = (
-        identifiant,
-        name,
-        typeClient,
-        netWorth,
-        orders,
-        credits
-      ) => {
-        return { identifiant , name, typeClient , netWorth , orders , credits};
-      }
-
-    const rows = [
-        createData(1,"Mthieu" ,"Agriculteur" , "48000$", 178 , 66 ),
-        createData(2,"Henry" ,"Agriculteur" , "167000$", 1211 , 312),
-        createData(3,"Pierre" ,"Agriculteur" , "1400$", 576, 102),
-        createData(4,"Bruno" ,"Cooperative" , "32000$" ,268,63),
-        createData(5,"Enubi" ,"Agriculteuse" , "82000$" ,768,333),
-        createData(6,"Hector" ,"Cooperative" , "122000$" ,368,123)
-    ];
+   
+   
 
   return (
-    <Container>
-        <MUINavbar />
-        <Main>
-            
-        </Main>
-    </Container>
+    <div className='dashboard'>
+      {/* header */}
+      <Header />
+
+      {/* app body  */}
+        {/* sidebar  */}
+        {/* feed  */}
+        {/* widgets  */}
+
+    </div>
   )
 }
 
