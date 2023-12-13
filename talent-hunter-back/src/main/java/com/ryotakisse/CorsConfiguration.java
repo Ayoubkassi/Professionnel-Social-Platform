8ad409a9a31d45c6a805d96a4db1fec3d727954f
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2023-12-13 21:41:45 +0100

    resolve cors problem

---
+++ talent-hunter-back/src/main/java/com/ryotakisse/CorsConfiguration.java
@@ -0,0 +1,25 @@
+package com.ryotakisse;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfiguer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**") // Define the mapping pattern
+                        .allowedOrigins("http://localhost:3000") // Replace with your frontend's origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
